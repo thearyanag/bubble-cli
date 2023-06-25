8dd431170c70c637407768e986d4b5bc734a6b94
aryan <agarwalaryan139@gmail.com>
2023-06-26 02:26:00 +0530

    add: upload and deploy commands

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ import clear from "clear";
 import chalk from "chalk";
 import figlet from "figlet";
 import args from "args-parser";
+import shell from "shelljs";
 let res = args(process.argv);
 
-import askCreate from "./lib/inquirer.js";
+// commands related to create module
+import { askCreate } from "./lib/create/inquirer.js";
+import { getFileContent } from "./lib/create/files.js";
+import { getAssetStats } from "./lib/create/stats.js";
+import { createConfig } from "./lib/create/createConfig.js";
 
-import { getFileContent , getAssetStats } from "./lib/files.js";
+// commands related to upload module
+import { askUpload } from "./lib/upload/inquirer.js";
+import { uploadImageToBundlr } from "./lib/upload/uploadImageToBundlr.js";
 
-clear();
+// commands related to deploy module
+import { createCollection } from "./lib/deploy/createCollection.js";
 
 if (Object.keys(res).length === 0) {
+    clear();
+
     console.log(
         chalk.whiteBright(
             figlet.textSync("Bubble", { horizontalLayout: "full" })
@@ -34,11 +44,31 @@ if (Object.keys(res).length === 0) {
     let keys = Object.keys(res)
     let command = keys[0];
     if (command === "create") {
-        getAssetStats();
-        console.log("create");
+
+        let stats = getAssetStats();
+        console.log()
+        let answers = await askCreate(stats);
+        let isConfigCreated = createConfig(answers, stats);
+        console.log()
+
+        if (isConfigCreated) {
+            console.log("Config file created successfully ✔️ \nPlease proceed to upload your assets ");
+        } else {
+            console.log("Config file creation failed");
+            shell.exit(1);
+        }
+
     } else if (command === "upload") {
+
+        let stats = getAssetStats();
+        console.log(stats)
+        let answers = await askUpload(stats);
+        let imageResponse = await uploadImageToBundlr(stats.no,answers);
+        console.log(answers)
+        console.log("Uploading assets to Arweave...");
         console.log("upload");
     } else if (command === "deploy") {
+        createCollection();
         console.log("deploy");
     } else {
         console.log("Invalid command");
